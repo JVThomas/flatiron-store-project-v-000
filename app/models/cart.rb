f734c6080140940caf7062fbbd11b0c749f7c5fe
Justin Thomas <jthomas0209@gmail.com>
2016-02-18 09:42:55 -0500

    Modified add_item to use queries

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,13 +4,20 @@ class Cart < ActiveRecord::Base
   belongs_to :user
 
   def add_item(item_id)
-    line_item = self.line_items.find_by(item_id: item_id)
+    line_item = LineItem.where(cart_id: self.id, item_id: item_id).first
     if !!line_item
       line_item.quantity += 1
     else
       line_item = self.line_items.build(item_id: item_id)
     end
     line_item
+    #line_item = self.line_items.find_by(item_id: item_id)
+    #if !!line_item
+    #  line_item.quantity += 1
+    #else
+    #  line_item = self.line_items.build(item_id: item_id)
+    #end
+    #line_item
   end
 
   def total
